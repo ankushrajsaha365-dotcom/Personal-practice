c7dd9aa5d878eb537cf29e8e94353975d281f475
Ankush Raj Saha <ankushrajsaha365@gmail.com>
2026-01-13 11:14:44 +0530

    Getting started with c++

---
+++ C++/helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    // std::cout << "Hello World!!!"<<std::endl;    //end of line 
+
+    // std::cout << "Youkoso!" <<'\n';
+    // std::cout << "Watashi no Soul society e";
+
+    std::cout << "Youkoso!\nWatshi no Soul Society e";
+
+
+    return 0;
+}
---
+++ C++/variable.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+
+
+int main(){
+    // integer datatype
+
+    // int x;
+    // x = 420;
+    // std::cout << x;
+
+    int x = 244;
+    int y = 37;
+
+    std::cout << x+y <<'\n';
+    // std::cout << x-y <<'\n';
+    // std::cout << x*y <<'\n';
+    // std::cout << x/y <<'\n';
+
+    //float and double
+
+    float x2 = 244.45;
+    float y2= 37.23;
+
+    std::cout << x2+y2 <<'\n';
+    // std::cout << x-y <<'\n';
+    // std::cout << x*y <<'\n';
+    // std::cout << x/y <<'\n';
+
+
+    double whole = 3532.34566;
+    std::cout << whole <<'\n';
+
+    //char datatype
+
+    char grade ='A';
+    std::cout <<grade <<'\n';
+
+    // char blood = "AB";                    two char simultaneouly cannot be accesed through char datatype
+    // std::cout << blood;
+
+    //boolean datatype
+
+    bool power = true;
+    std::cout << power <<'\n';
+
+    //string datatype
+    std::string name = "Raj";
+    std::cout <<name;
+
+    return 0;
+} 

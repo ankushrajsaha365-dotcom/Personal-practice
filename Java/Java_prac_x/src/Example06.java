0258fd845d6da4f70e0cd48f62849de95c3fbc9a
Ankush Raj Saha <ankushrajsaha365@gmail.com>
2026-03-22 10:36:15 +0530

    Java codes

--- Java/Java_prac_x/src/Example06.java
+++ Java/Java_prac_x/src/Example06.java
@@ -2,7 +2,7 @@ public class Example06 {
     public static void main(String[] args){
         double a = 3.0 , b = 4.0;
         double c = Math.sqrt(a*a+b*b);
-        System.out.println("Hypoyenuse is "+c);
+        System.out.println("Hypotenuse is "+c);
     }
     
 }
---
+++ Java/Java_practice1/src/JAVA_06_Operators.java
@@ -0,0 +1,20 @@
+public class JAVA_06_Operators {
+    public static void main(String[] args){
+        int a = 34;                        //Arithmetic operator
+        int a2 = a*3;
+        System.out.println(a2);
+        
+        
+        int b = 56;                        //Assignment operator
+        b *= 3 ;                        
+        System.out.println(b);
+
+        System.out.println(6>8);           //Comparison operator
+
+        System.out.println(12>3 || 4>6);   //Logical operator
+        System.out.println(12>3 && 4>6);
+
+        System.out.println(2&3);           //Bitwise operator
+    }
+}
+    

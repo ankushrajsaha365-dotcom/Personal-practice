40dd7dae67960e1ae18c83c4e08fcf4fe58182e1
ankushrajsaha365-dotcom <ankushrajsaha365@gmail.com>
2026-06-11 22:39:45 +0530

    Practice more JAVA

--- Java/lets_java/Prac4.java
+++ Java/lets_java/Prac4.java
@@ -1,5 +1,32 @@
 package Java.lets_java;
-
-public class Prac4 {
     
+// First Interface with a default method
+interface Dog {
+    default void sample() {
+        System.out.println("Wofoo");
+    }
+}
+
+// Second Interface with a default method
+interface Cat {
+    default void sample() {
+        System.out.println("Meow");
+    }
+}
+
+// Class implementing both interfaces
+class Prac4 implements Dog, Cat {
+
+    // You write the method ONLY ONCE to resolve the conflict
+    public void sample() {
+        // You can choose to run both implementations inside here:
+        Dog.super.sample(); // Calls Dog's sample()
+        Cat.super.sample(); // Calls Cat's sample()
+    }
+
+    // Main Method
+    public static void main(String args[]) {
+        Prac4 obj = new Prac4();
+        obj.sample(); // This will trigger both prints via the overridden method
+    }
 }
